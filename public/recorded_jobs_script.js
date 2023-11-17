c770d9f68bb711c1dbc09599583d93907d6eeb17
LZsolar <pornchanok.char@mail.kmutt.ac.th>
2023-11-17 20:05:42 +0700

    Add many more of CSS

--- public/recorded_jobs_script.js
+++ public/recorded_jobs_script.js
@@ -40,11 +40,33 @@ function displayRecordedJobs(jobs) {
                 <p>Reward: ${job.Reward}</p>
                 <p>Ghost Type: ${job['Ghost type']}</p>
                 <!-- Add more job details as needed -->
+                <button onclick="applyForJob(${job.id})">More!</button>
             </div>
         `).join('');
 
         recordedJobsSection.innerHTML = jobListHTML;
     }
+}
+
+function fetchJobData(callback) {
+    // You would typically make an API call to the server here
+    // For simplicity, let's assume the server responds with a JSON array
+
+    // For demonstration purposes, we'll fetch data from the jobs.json file
+    fetch('jobs.json')
+        .then(response => response.json())
+        .then(data => callback(data))
+        .catch(error => console.error('Error fetching job data:', error));
+}
+function applyForJob(jobId) {
+    // Find the job by id and perform the application logic
+    fetchJobData(jobs => {
+        const selectedJob = jobs.find(job => job.id === jobId);
+        if (selectedJob) {
+            // Move to the next page with the job details
+            window.location.href = `jobdetail.html?id=${selectedJob.id}`;
+        }
+    });
 }
 
 // Function to get a cookie value by name
